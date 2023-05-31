c3395d4d89a951c0a59f513e8b8fcaa669619fdd
John Doe <johndoe@example.com>
2023-06-01 07:35:38 +0900

    run test.py

--- test.py
+++ test.py
@@ -1,8 +1,2 @@
 import numpy as np
 print("Hello world.")
-
-x = np.random.randn(1000, 2)
-output = "\n".join([",".join(_.astype(str)) for _ in x])
-
-with open("results.csv", "w") as o:
-    o.write(output)
